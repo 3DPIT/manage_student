7e2bb2aaa6fa9401f187bb0961b2ac888a383fd9
Black <oksos0817@naver.com>
2024-10-24 07:54:48 +0900

    feat : up code backup

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,7 +1,9 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
 import org.fastcampus.student_management.ui.course.CourseController;
@@ -24,6 +26,17 @@ public class Main {
 
     CourseController courseController = new CourseController(coursePresenter, courseService, studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    StudentInfoDto studentInfoDto = new StudentInfoDto("Kim", 20, "Seoul");
+    StudentInfoDto studentInfoDto2 = new StudentInfoDto("Lee", 25, "Busan");
+    studentService.saveStudent(studentInfoDto);
+    studentService.saveStudent(studentInfoDto2);
+
+    CourseInfoDto courseInfoDto = new CourseInfoDto("Math",1000, "MONDAY", "Kim", 10L);
+    CourseInfoDto courseInfoDto2 = new CourseInfoDto("English",2000, "TUESDAY", "Lee", 20L);
+    courseService.registerCourse(courseInfoDto);
+    courseService.registerCourse(courseInfoDto2);
+
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
